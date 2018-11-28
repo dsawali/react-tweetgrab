32b05e2d725c7f90f33755610aa808f385ea4063
wilhans <hansenwilliam0225@gmail.com>
2018-11-27 22:48:15 -0800

    Added the connection between back end and front end
    Added webpack config to run reactjs
    Added the server and the twitter api call
    Refactored some unused file
    Added some dependencies
    Restructured the file system

---
+++ src/js/Container.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    window.twttr = (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+            t = window.twttr || {};
+        if (d.getElementById(id)) return t;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+
+        t._e = [];
+        t.ready = function (f) {
+            t._e.push(f);
+        };
+
+        return t;
+    }(document, "script", "twitter-wjs"));
+});
+
+export default class Container extends Component {
+      render() {
+          return(
+              <div className="display-row">
+                  <SearchBox />
+                  <OptionBox />
+              </div>
+          );
+      }
+  }
+  
+  class SearchBox extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                query: ''
+            }
+            this.handleChange = this.handleChange.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
+        }
+        handleChange(event) {
+            this.setState({query: event.target.value});
+        }
+
+        handleSubmit(event) {
+            event.preventDefault();
+            alert('Query is : ' + this.state.query);
+            let searchData = {"searchData":this.state.query};
+            fetch('/search', {
+                method: 'POST',
+                body: JSON.stringify(searchData),
+                headers: {"Content-Type": "application/json"}
+            }).then((response) => {
+                return response.json();
+            }).then((responseJson) => {
+                createTweet(responseJson.id);
+            });
+        }
+
+        render() {
+            return (
+              <div>
+                  <form onSubmit={this.handleSubmit}>
+                      <input type="text" className="search-box" onChange={this.handleChange} value={this.state.value} />
+                  </form>
+              </div>
+            );
+        }
+    }
+    class OptionBox extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                context: 'hashtag'
+            };
+            this.handleChange = this.handleChange.bind(this);
+        }
+
+        handleChange(event) {
+            this.setState({context: event.target.value});
+            let optionData = {"optionData": event.target.value};
+            fetch('/option',{
+                method: 'POST',
+                body: JSON.stringify(optionData),
+                headers: {"Content-Type": "application/json"}
+            });
+        }
+
+        render() {
+            return (
+                <div>
+                    <select className="search-context" onChange={this.handleChange} value={this.state.value}>
+                        <option value="hashtag">hashtag</option>
+                        <option value="user">user</option>
+                        <option value="text">text</option>
+                    </select>
+                </div>
+            );
+        }
+  }
+
+  function createTweet(id) {
+    let wrapperClass = document.getElementById('tweet-wrapper');
+    let containerId = 'tweet-' + (document.getElementsByClassName('tweet-container')).length;
+    let tweetContainer = '<div class=\"tweet-container\" id=\"' + containerId + '\"></div>';
+    wrapperClass.innerHTML += tweetContainer;
+
+    twttr.widgets.createTweet(id,
+        document.getElementById(containerId),
+        {
+            align: 'center'
+        });
+}
