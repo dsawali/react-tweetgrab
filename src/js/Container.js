8b3c6f46f67d1389776fa9a284585a27e45b6f71
wilhans <hansenwilliam0225@gmail.com>
2018-11-29 23:46:03 -0800

    Resolved single tweet bug
    Resolving bug about calls can only show one tweets.
    Added scrolling bar function.

--- src/js/Container.js
+++ src/js/Container.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function (event) {
     window.twttr = (function (d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0],
             t = window.twttr || {};
@@ -20,95 +20,110 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 export default class Container extends Component {
-      render() {
-          return(
-              <div className="display-row">
-                  <SearchBox />
-                  <OptionBox />
-              </div>
-          );
-      }
-  }
-  
-  class SearchBox extends Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                query: ''
-            }
-            this.handleChange = this.handleChange.bind(this);
-            this.handleSubmit = this.handleSubmit.bind(this);
-        }
-        handleChange(event) {
-            this.setState({query: event.target.value});
-        }
-
-        handleSubmit(event) {
-            event.preventDefault();
-            alert('Query is : ' + this.state.query);
-            let searchData = {"searchData":this.state.query};
-            fetch('/search', {
-                method: 'POST',
-                body: JSON.stringify(searchData),
-                headers: {"Content-Type": "application/json"}
-            }).then((response) => {
-                return response.json();
-            }).then((responseJson) => {
-                createTweet(responseJson.id);
-            });
-        }
-
-        render() {
-            return (
-              <div>
-                  <form onSubmit={this.handleSubmit}>
-                      <input type="text" className="search-box" onChange={this.handleChange} value={this.state.value} />
-                  </form>
-              </div>
-            );
-        }
+    render() {
+        return (
+            <div className="display-row">
+                <SearchBox />
+                <OptionBox />
+            </div>
+        );
     }
-    class OptionBox extends Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                context: 'hashtag'
-            };
-            this.handleChange = this.handleChange.bind(this);
-        }
+}
 
-        handleChange(event) {
-            this.setState({context: event.target.value});
-            let optionData = {"optionData": event.target.value};
-            fetch('/option',{
-                method: 'POST',
-                body: JSON.stringify(optionData),
-                headers: {"Content-Type": "application/json"}
-            });
+class SearchBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({ query: event.target.value });
+    }
 
-        render() {
-            return (
-                <div>
-                    <select className="search-context" onChange={this.handleChange} value={this.state.value}>
-                        <option value="hashtag">hashtag</option>
-                        <option value="user">user</option>
-                        <option value="text">text</option>
-                    </select>
-                </div>
-            );
-        }
-  }
+    handleSubmit(event) {
+        event.preventDefault();
+        // alert('Query is : ' + this.state.query);
+        let searchData = { "searchData": this.state.query };
+        fetch('/search', {
+            method: 'POST',
+            body: JSON.stringify(searchData),
+            headers: { "Content-Type": "application/json" }
+        }).then((response) => {
+            return response.json();
+        }).then((responseJson) => {
+            const numberRegex = /^[0-9]*$/;
+            let id = responseJson.id;
+            if (id.match(numberRegex)) {
+                createTweet(id);
+            } else {
+                alert(id);
+            }
+        });
+    }
 
-  function createTweet(id) {
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" className="search-box" onChange={this.handleChange} value={this.state.value} />
+                </form>
+            </div>
+        );
+    }
+}
+class OptionBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            context: 'hashtag'
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ context: event.target.value });
+        let optionData = { "optionData": event.target.value };
+        fetch('/option', {
+            method: 'POST',
+            body: JSON.stringify(optionData),
+            headers: { "Content-Type": "application/json" }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <select className="search-context" onChange={this.handleChange} value={this.state.value}>
+                    <option value="hashtag">hashtag</option>
+                    <option value="user">user</option>
+                    <option value="text">text</option>
+                </select>
+            </div>
+        );
+    }
+}
+
+function createTweet(id) {
     let wrapperClass = document.getElementById('tweet-wrapper');
     let containerId = 'tweet-' + (document.getElementsByClassName('tweet-container')).length;
-    let tweetContainer = '<div class=\"tweet-container\" id=\"' + containerId + '\"></div>';
-    wrapperClass.innerHTML += tweetContainer;
+
+    //Create new div element
+    let tweetContainer = document.createElement('div');
+    tweetContainer.class = 'tweet-container';
+    tweetContainer.id = containerId;
+
+    //To prepend
+    wrapperClass.insertBefore(tweetContainer, wrapperClass.childNodes[0]);
 
     twttr.widgets.createTweet(id,
         document.getElementById(containerId),
         {
+            width: '407px',
             align: 'center'
         });
+
+
 }
